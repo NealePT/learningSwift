a6472a96885236ad02397fee76a2e656bf1e4a71
Neale Taylor <nealeptaylor@gmail.com>
2022-09-21 15:51:19 -0400

    create display for view box

--- DoseTrack/ViewController.swift
+++ DoseTrack/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: NSViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    override func loadView() {
+        self.view = NSView(frame: NSRect(x: 0, y: 0, width: 400, height: 200))
+    }
 
     override var representedObject: Any? {
         didSet {
